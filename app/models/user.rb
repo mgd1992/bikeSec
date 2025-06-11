8dddf715947d421cf07695452a6d1b2d7de9f188
marco garbini <marcoogarbini@gmail.com>
2025-06-11 14:34:16 +0200

    darle algo de estilo al form para crear usuarios. agregar validaciones en los campos

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
   has_many :bike_services, dependent: :destroy
+
+  validates :nombre, :apellido, presence: true
+  validates :email, uniqueness: true, presence: true
+  validates :telefono, format: { with: /\A(\+34|0034|34)?[6789]\d{8}\z/, message: "no es un número de teléfono válido" }
+
+
 end
