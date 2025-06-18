3405915de6b0ff1bd8ba224826307d7f3e48d262
marco garbini <marcoogarbini@gmail.com>
2025-06-18 10:28:35 +0200

    modificando datepicker.js y servicePdf.rb

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,8 +1,8 @@
-// Import and register all your controllers from the importmap via controllers/**/*_controller
+
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 
-import DatepickerController from "./datepicker_controller"
+import DatepickerController from "datepicker_controller"
 
 application.register("datepicker", DatepickerController)
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,11 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2025_06_03_141505) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "bike_services", force: :cascade do |t|
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.string "descripcion", null: false
     t.date "date", null: false
     t.decimal "cost"
