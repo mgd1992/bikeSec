7af8fcca24886c13dd7a0ae4e8976e9130aae98e
marco garbini <marcoogarbini@gmail.com>
2025-06-20 10:19:25 +0200

    Modificando la logica del admin form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
 
     if params[:email].blank? || params[:password].blank?
       flash.now[:alert] = "Por favor revisa todos los campos"
-      render :new
+      redirect_to login_path
       return
     end
 
